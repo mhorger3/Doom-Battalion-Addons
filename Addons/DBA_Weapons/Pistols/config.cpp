bf50fe42b84d743dbb19cc47419068ea1ffe0f2b
Frisk <bmxcaleb@gmail.com>
2021-05-06 14:33:47 -0500

    Adding A180
    THis guns sucks ass cause it shoots 9mm rn

--- Addons/DBA_Weapons/Pistols/config.cpp
+++ Addons/DBA_Weapons/Pistols/config.cpp
@@ -2,12 +2,12 @@ class cfgPatches
 {
 	class DBA_Aux_Mod_DC17
 	{
-		author = "Frisk";
+		author = "Trip";
 		name = "101st Aux Mod Weapons";
 		requiredaddons[] = {"JLTS_weapons_DC15S","101st_Aux_Mod","DBA_patch_weapons"};
 		requiredversion = 0.1;
 		units[] = {};
-		weapons[] = {"DBA_DC17","DBA_Cannok","DBA_Defender","DBA_Westar35_Mod_0","DBA_Dual_Westar35_Mod_0", "DBA_Dual_Defender"};
+		weapons[] = {"DBA_A180","DBA_DC17","DBA_Cannok","DBA_Defender","DBA_Westar35_Mod_0","DBA_Dual_Westar35_Mod_0", "DBA_Dual_Defender"};
 	};
 };
 class UGL_F;
@@ -82,7 +82,7 @@ class CfgWeapons
 		magazineWell[] = {};
 		modes[] = {"Single","Burst","FullAuto"};*/
 
-		author = "Frisk";
+		author = "Trip";
 		scope = 0; // Change this to 2
 		picture = "\MRC\JLTS\weapons\DC17SA\data\ui\DC17SA_ui_ca.paa";
 
@@ -508,7 +508,7 @@ class CfgWeapons
 	};
 	class DBA_Defender: DBA_Pistol_Base
 	{
-		author="Frisk";
+		author="Trip";
 		scope=2;
 		JLTS_canHaveShield=1;
 		baseweapon="DBA_Defender";
@@ -822,13 +822,175 @@ class CfgWeapons
 			mass=60;
 		};
 	};
+	class DBA_A180:DBA_Pistol_Base
+	{
+		//Make sure you add these somewhere or its probably not going to work
+		
+		JLTS_canHaveShield = 0;
+		baseWeapon = "DBA_A180";
+		model = "101st_Aux_Mod\Addons\DBA_Weapons\Pistols\A180.p3d";
+		magazines[] = {"DBA_A180_Mag_x20"};
+		magazineWell[] = {};
+		modes[] = {"Single","Burst","FullAuto"};
+
+		author = "Trip";
+		scope = 2; // Change this to 2
+		picture = "\MRC\JLTS\weapons\DC17SA\data\ui\DC17SA_ui_ca.paa";
+
+		selectionFireAnim = "zasleh";
+		flash = "gunfire";
+		flashSize = 0.5;
+		fireLightDiffuse[] = {.7,0,.7};
+		fireLightAmbient[] = {.7,0,.7};
+		fireLightIntensity = 0.2;
+		fireLightDuration = 0.05;
+		drySound[] = {"MRC\JLTS\weapons\Core\sounds\weapon_dry.wss",5,1,10};
+		muzzles[] = {"this","Stun"};
+		class Stun: JLTS_stun_muzzle
+		{
+			reloadAction = "GestureReloadPistol";
+			reloadMagazineSound[] = {"A3\Sounds_F\arsenal\weapons\Pistols\P07\reload_P07",1,1,10};
+		};
+		class Single: Mode_SemiAuto
+		{
+			sounds[] = {"StandardSound"};
+			class BaseSoundModeType
+			{
+				weaponSoundEffect = "";
+				closure1[] = {};
+				closure2[] = {};
+				soundClosure[] = {};
+			};
+			class StandardSound: BaseSoundModeType
+			{
+				weaponSoundEffect="";
+				begin1[]=
+				{
+					"MRC\JLTS\weapons\RG4D\sounds\RG4D_fire",
+					3,
+					1,
+					1800
+				};
+				soundBegin[]=
+				{
+					"begin1",
+					1
+				};
+			};
+			recoil = "recoil_pistol_light";
+			recoilProne = "recoil_prone_pistol_light";
+			reloadTime = 0.12;
+			dispersion = 0.0029;
+			minRange = 5;
+			minRangeProbab = 0.3;
+			midRange = 25;
+			midRangeProbab = 0.6;
+			maxRange = 50;
+			maxRangeProbab = 0.1;
+			aiRateOfFire = 2;
+			aiRateOfFireDistance = 25;
+		};
+		class Burst: Mode_Burst
+		{
+			sounds[] = {"StandardSound"};
+			class BaseSoundModeType
+			{
+				weaponSoundEffect = "";
+				closure1[] = {};
+				closure2[] = {};
+				soundClosure[] = {};
+			};
+			class StandardSound: BaseSoundModeType
+			{
+				weaponSoundEffect = "";
+				begin1[] = {"MRC\JLTS\weapons\DC17SA\sounds\dc17sa_fire_2",3,0.9,1800};
+				soundBegin[] = {"begin1",1};
+			};
+			recoil = "recoil_pistol_light";
+			recoilProne = "recoil_prone_pistol_light";
+			reloadTime = 0.12;
+			dispersion = 0.0029;
+			minRange = 5;
+			minRangeProbab = 0.3;
+			midRange = 25;
+			midRangeProbab = 0.6;
+			maxRange = 50;
+			maxRangeProbab = 0.1;
+			aiRateOfFire = 2;
+			aiRateOfFireDistance = 25;
+		};
+		class FullAuto: Mode_FullAuto
+		{
+			sounds[] = {"StandardSound"};
+			class BaseSoundModeType
+			{
+				weaponSoundEffect = "";
+				closure1[] = {};
+				closure2[] = {};
+				soundClosure[] = {};
+			};
+			class StandardSound: BaseSoundModeType
+			{
+				weaponSoundEffect = "";
+				begin1[] = {"MRC\JLTS\weapons\DC17SA\sounds\dc17sa_fire_2",3,0.9,1800};
+				soundBegin[] = {"begin1",1};
+			};
+			recoil = "recoil_pistol_light";
+			recoilProne = "recoil_prone_pistol_light";
+			reloadTime = 0.12;
+			dispersion = 0.0029;
+			minRange = 5;
+			minRangeProbab = 0.3;
+			midRange = 25;
+			midRangeProbab = 0.6;
+			maxRange = 50;
+			maxRangeProbab = 0.1;
+			aiRateOfFire = 2;
+			aiRateOfFireDistance = 25;
+		};
+		class WeaponSlotsInfo: WeaponSlotsInfo
+		{
+			mass = 20;
+			class CowsSlot: CowsSlot
+			{
+				compatibleItems[] = {};
+			};
+			class PointerSlot: PointerSlot
+			{
+				compatibleItems[] = {};
+			};
+			class MuzzleSlot: MuzzleSlot
+			{
+				linkProxy = "\A3\data_f\proxies\weapon_slots\MUZZLE";
+				compatibleItems[] = {};
+				iconPosition[] = {0.24,0.35};
+				iconScale = 0.2;
+			};
+			class UnderBarrelSlot: UnderBarrelSlot
+			{
+				compatibleItems[] = {};
+			};
+		};
+
+		//Ace overheat shit do not touch
+
+		ACE_overheating_allowSwapBarrel = 1;
+		ACE_Overheating_Dispersion = 0;
+		ACE_Overheating_JamChance = 0;
+		ACE_overheating_mrbs = 3e+09;
+		ACE_overheating_slowdownFactor = 0;
+		ace_overpressure_angle = 0;
+		ace_overpressure_damage = 0;
+		ace_overpressure_priority = 1;
+		ace_overpressure_range = 0;
+	};
 };
 class CfgMagazines
 {
 	class CA_Magazine;
 	class Dual_Hammerhead_x9_Magazine: CA_Magazine
 	{
-		author="Frisk";
+		author="Trip";
 		scope=2;
 		displayName = "9Rnd Dual Hammerhead Magazine";
 		picture = "\MRC\JLTS\weapons\DC17SA\data\ui\DC17SA_mag_ui_ca.paa";
@@ -844,7 +1006,7 @@ class CfgMagazines
 	
 	class Dual_Defender_x22_Magazine: CA_Magazine
 	{
-		author="Frisk";
+		author="Trip";
 		scope=2;
 		displayName="101st PDW Dual Defender Magazine";
 		ammo="101st_PDW_Balls";
@@ -859,6 +1021,19 @@ class CfgMagazines
 		modelSpecial="101st_Aux_Mod\Addons\DBA_Weapons\Pistols\Dual\DualDefenderMag.p3d";
 		modelSpecialIsProxy = 1;
 	};
+	class DBA_A180_Mag_x20: CA_Magazine
+	{
+		author="Trip";
+		scope=2;
+		displayName = "20Rnd A180 Magazine";
+		picture = "\MRC\JLTS\weapons\DC17SA\data\ui\DC17SA_mag_ui_ca.paa";
+		ammo="DBA_Purple_9_Ammo";
+		count=20;
+		mass=4;
+		initSpeed=400;
+		typicalSpeed=400;
+		tracersEvery=1;
+	};
 };
 class CfgAmmo
 {
@@ -869,13 +1044,18 @@ class CfgAmmo
 		hit = 50;
 		caliber = 5;
 	};
+	class 442_ammo_9_purple;
+	class DBA_Purple_9_Ammo:442_ammo_9_purple
+	{
+		airFriction=0;
+	};
 };
 /*class CfgVehicles
 {
 	class Weapon_Base_F;
 	class DBA_VH_DC17: Weapon_Base_F
 	{
-		author = "Frisk";
+		author = "Trip";
 		displayName = "$STR_DBA_Maalras";
 		scope = 2;
 		class TransportWeapons
