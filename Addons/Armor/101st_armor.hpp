741f523951323e90a7589f89b693bf3a0d4da731
VulgarWorlock <jakelane200@gmail.com>
2021-05-09 02:27:47 +0100

    Spork Armour - need to move to main branch but lazy

--- Addons/Armor/101st_armor.hpp
+++ Addons/Armor/101st_armor.hpp
@@ -2049,6 +2049,41 @@ class 101st_212_Quill: Uniform_Base
 		mass=40;
 	};
 };
+class 101st_212_Spark : Uniform_Base
+{
+	dlc = "101st_Aux_Mod";
+	scope = 2;
+	displayName = "101st Spark (JLTS)";
+	picture = "\MRC\JLTS\characters\CloneArmor\data\ui\CloneArmor_ui_ca.paa";
+	model = "\A3\Characters_F\Common\Suitpacks\suitpack_blufor_diver";
+	hiddenSelections[] =
+	{
+		"camo1",
+		"camo2"
+	};
+	hiddenSelectionsTextures[] =
+	{
+		"\101st_Aux_Mod\Addons\textures\101st\armor\Spark_Armor_upper_CO.paa",
+		"\101st_Aux_Mod\Addons\textures\101st\armor\Spark_Armor_lower_CO.paa"
+	};
+	hiddenSelectionsMaterials[] = {
+		"\101st_Aux_Mod\Addons\textures\101st\armor\data\Spark_Upper.rvmat",
+		"\101st_Aux_Mod\Addons\textures\101st\armor\data\Spark_Lower.rvmat"
+	};
+	class ItemInfo : UniformItem
+	{
+		uniformModel = "-";
+		uniformClass = "101st_Quill_212th";
+		containerClass = "supply150";
+		uniformType = "Neopren";
+		hiddenSelections[] =
+		{
+			"camo",
+			"camo1"
+		};
+		mass = 40;
+	};
+};
 class 101st_212_Seatie: Uniform_Base
 {
 	dlc = "101st_Aux_Mod";
