8538898f36a1bab4a09c823cb1bb33c3d9eb6ffd
Frisk <bmxcaleb@gmail.com>
2021-05-09 09:25:38 -0500

    Adding Test Pistol

--- Addons/DBA_Weapons/Pistols/config.cpp
+++ Addons/DBA_Weapons/Pistols/config.cpp
@@ -7,7 +7,7 @@ class cfgPatches
 		requiredaddons[] = {"JLTS_weapons_DC15S","101st_Aux_Mod","DBA_patch_weapons"};
 		requiredversion = 0.1;
 		units[] = {};
-		weapons[] = {"DBA_A180","DBA_DC17","DBA_Cannok","DBA_Defender","DBA_Westar35_Mod_0","DBA_Dual_Westar35_Mod_0", "DBA_Dual_Defender"};
+		weapons[] = {"DBA_Test_Pistol","DBA_A180","DBA_DC17","DBA_Cannok","DBA_Defender","DBA_Westar35_Mod_0","DBA_Dual_Westar35_Mod_0", "DBA_Dual_Defender"};
 	};
 };
 class UGL_F;
@@ -1022,6 +1022,211 @@ class CfgWeapons
 				compatibleItems[] = {};
 			};
 		};
+
+		//Ace overheat shit do not touch
+
+		ACE_overheating_allowSwapBarrel = 1;
+		ACE_Overheating_Dispersion = 0;
+		ACE_Overheating_JamChance = 0;
+		ACE_overheating_mrbs = 3e+09;
+		ACE_overheating_slowdownFactor = 0;
+		ace_overpressure_angle = 0;
+		ace_overpressure_damage = 0;
+		ace_overpressure_priority = 1;
+		ace_overpressure_range = 0;
+	};
+	class DBA_Test_Pistol:DBA_Pistol_Base
+	{
+		//Make sure you add these somewhere or its probably not going to work
+		
+		displayName = "[101st] TESTING WEAPON ONLY";
+		JLTS_canHaveShield = 0;
+		baseWeapon = "DBA_Test_Pistol";
+		model = "101st_Aux_Mod\Addons\DBA_Weapons\Pistols\A180.p3d";
+		magazines[] = {};
+		magazineWell[] = 
+		{ 
+			"DBA_DC17_GL_Mags_magwell",
+			"DBA_Test_magwell" 
+		};
+		modes[] = {"Single","Burst","FullAuto"};
+
+		author = "Trip";
+		scope = 2; // Change this to 2
+		picture = "\MRC\JLTS\weapons\DC17SA\data\ui\DC17SA_ui_ca.paa";
+
+		selectionFireAnim = "zasleh";
+		flash = "gunfire";
+		flashSize = 0.5;
+		fireLightDiffuse[] = {.7,0,.7};
+		fireLightAmbient[] = {.7,0,.7};
+		fireLightIntensity = 0.2;
+		fireLightDuration = 0.05;
+		drySound[] = {"MRC\JLTS\weapons\Core\sounds\weapon_dry.wss",5,1,10};
+		muzzles[] = {"this","Stun"};
+		class Stun: JLTS_stun_muzzle
+		{
+			reloadAction = "GestureReloadPistol";
+			reloadMagazineSound[] = {"A3\Sounds_F\arsenal\weapons\Pistols\P07\reload_P07",1,1,10};
+		};
+		class Single: Mode_SemiAuto
+		{
+			sounds[] = {"StandardSound"};
+			class BaseSoundModeType
+			{
+				weaponSoundEffect = "";
+				closure1[] = {};
+				closure2[] = {};
+				soundClosure[] = {};
+			};
+			class StandardSound: BaseSoundModeType
+			{
+				weaponSoundEffect="";
+				begin1[]=
+				{
+					"MRC\JLTS\weapons\RG4D\sounds\RG4D_fire",
+					3,
+					1,
+					1800
+				};
+				soundBegin[]=
+				{
+					"begin1",
+					1
+				};
+			};
+			recoil = "recoil_pistol_light";
+			recoilProne = "recoil_prone_pistol_light";
+			reloadTime = 0.12;
+			dispersion = 0.0029;
+			minRange = 5;
+			minRangeProbab = 0.3;
+			midRange = 25;
+			midRangeProbab = 0.6;
+			maxRange = 50;
+			maxRangeProbab = 0.1;
+			aiRateOfFire = 2;
+			aiRateOfFireDistance = 25;
+		};
+		class Burst: Mode_Burst
+		{
+			sounds[] = {"StandardSound"};
+			class BaseSoundModeType
+			{
+				weaponSoundEffect = "";
+				closure1[] = {};
+				closure2[] = {};
+				soundClosure[] = {};
+			};
+			class StandardSound: BaseSoundModeType
+			{
+				weaponSoundEffect = "";
+				begin1[]=
+				{
+					"MRC\JLTS\weapons\RG4D\sounds\RG4D_fire",
+					3,
+					1,
+					1800
+				};
+				soundBegin[] = {"begin1",1};
+			};
+			recoil = "recoil_pistol_light";
+			recoilProne = "recoil_prone_pistol_light";
+			reloadTime = 0.12;
+			dispersion = 0.0029;
+			minRange = 5;
+			minRangeProbab = 0.3;
+			midRange = 25;
+			midRangeProbab = 0.6;
+			maxRange = 50;
+			maxRangeProbab = 0.1;
+			aiRateOfFire = 2;
+			aiRateOfFireDistance = 25;
+		};
+		class FullAuto: Mode_FullAuto
+		{
+			sounds[] = {"StandardSound"};
+			class BaseSoundModeType
+			{
+				weaponSoundEffect = "";
+				closure1[] = {};
+				closure2[] = {};
+				soundClosure[] = {};
+			};
+			class StandardSound: BaseSoundModeType
+			{
+				weaponSoundEffect = "";
+				begin1[]=
+				{
+					"MRC\JLTS\weapons\RG4D\sounds\RG4D_fire",
+					3,
+					1,
+					1800
+				};
+				soundBegin[] = {"begin1",1};
+			};
+			recoil = "recoil_pistol_light";
+			recoilProne = "recoil_prone_pistol_light";
+			reloadTime = 0.12;
+			dispersion = 0.0029;
+			minRange = 5;
+			minRangeProbab = 0.3;
+			midRange = 25;
+			midRangeProbab = 0.6;
+			maxRange = 50;
+			maxRangeProbab = 0.1;
+			aiRateOfFire = 2;
+			aiRateOfFireDistance = 25;
+		};
+		class WeaponSlotsInfo: WeaponSlotsInfo
+		{
+			mass = 20;
+			class CowsSlot: CowsSlot
+			{
+				compatibleItems[] = {};
+			};
+			class PointerSlot: PointerSlot
+			{
+				compatibleItems[] = {};
+			};
+			class MuzzleSlot: MuzzleSlot
+			{
+				linkProxy = "\A3\data_f\proxies\weapon_slots\MUZZLE";
+				compatibleItems[] = {};
+				iconPosition[] = {0.24,0.35};
+				iconScale = 0.2;
+			};
+			class UnderBarrelSlot: UnderBarrelSlot
+			{
+				compatibleItems[] = {};
+			};
+		};
+		class FlashLight
+        {
+            color[]={180,60,60}; //color[]={180,160,130}; Default
+            ambient[]={0.9,0.3,0.3}; //ambient[]={0.89999998,0.80000001,0.69999999}; Default
+            intensity=100;
+            size=1;
+            innerAngle=5;
+            outerAngle=100;
+            coneFadeCoef=10;
+            position="konec hlavne";
+            direction="usti hlavne";
+            useFlare=1;
+            flareSize=1.5;
+            flareMaxDistance=100;
+            dayLight=1;
+            class Attenuation
+            {
+                start=0;
+                constant=0.5;
+                linear=0.1;
+                quadratic=0.2;
+                hardLimitStart=27;
+                hardLimitEnd=34;
+            };
+            scale[]={0};
+        };
 
 		//Ace overheat shit do not touch
 
