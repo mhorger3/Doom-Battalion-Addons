53ab9b080868c66213780acfd4b0871a864c3c80
Spark <82785774+SparkyWorlock@users.noreply.github.com>
2021-05-15 10:10:32 -0400

    Adding Helms & Armor | Tweaking Guns

--- Addons/Armor/101st_armor.hpp
+++ Addons/Armor/101st_armor.hpp
@@ -2115,6 +2115,37 @@ class 101st_212_Spark : Uniform_Base
 		mass = 40;
 	};
 };
+class 101st_212_Oaksy : Uniform_Base
+{
+	dlc = "101st_Aux_Mod";
+	scope = 2;
+	displayName = "101st Oaksy (JLTS)";
+	picture = "\MRC\JLTS\characters\CloneArmor\data\ui\CloneArmor_ui_ca.paa";
+	model = "\A3\Characters_F\Common\Suitpacks\suitpack_blufor_diver";
+	hiddenSelections[] =
+	{
+		"camo1",
+		"camo2"
+	};
+	hiddenSelectionsTextures[] =
+	{
+		"\101st_Aux_Mod\Addons\textures\101st\armor\Oaksy_chest_CO.paa",
+		"\101st_Aux_Mod\Addons\textures\101st\armor\Oaksy_legs_CO.paa"
+	};
+	class ItemInfo : UniformItem
+	{
+		uniformModel = "-";
+		uniformClass = "101st_Oaksy_212th";
+		containerClass = "supply150";
+		uniformType = "Neopren";
+		hiddenSelections[] =
+		{
+			"camo",
+			"camo1"
+		};
+		mass = 40;
+	};
+};
 class 101st_212_Dolittle : Uniform_Base
 {
 	dlc = "101st_Aux_Mod";
