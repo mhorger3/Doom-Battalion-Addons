12305011055da7066df8d664e503dfef519dc1c8
Frisk <bmxcaleb@gmail.com>
2021-05-16 12:49:22 -0500

    Eweb Fixes

--- Addons/DBA_CIS/Turrets/config.cpp
+++ Addons/DBA_CIS/Turrets/config.cpp
@@ -71,7 +71,7 @@ class CfgVehicles
 		{
 			description="";
 		};
-		armor=400;
+		armor=100;
 		model="101st_Aux_Mod\Addons\DBA_CIS\Turrets\eweb.p3d";
 		editorSubcategory="EdSubcat_Turrets";
 		picture="\A3\Static_f_gamma\data\ui\gear_StaticTurret_MG_CA.paa";
@@ -146,18 +146,7 @@ class CfgVehicles
 				gunnergetOutAction="";
 				GunnerName="Gunner";
 				gunnerForceOptics=0;
-				hiddenSelections[]=
-				{
-					"camo1"
-				};
-				hiddenSelectionsTextures[]=
-				{
-					"3as\3as_static\StationaryTurret\Data\Textures\turret2_co.paa"
-				};
-				hiddenSelectionsMaterials[]=
-				{
-					"3as\3as_static\StationaryTurret\Data\Textures\turret.rvmat"
-				};
+				
 				memoryPointsGetInGunner="pos_gunner";
 				memoryPointsGetInGunnerDir="pos_gunner_dir";
 				class ViewOptics: ViewOptics
