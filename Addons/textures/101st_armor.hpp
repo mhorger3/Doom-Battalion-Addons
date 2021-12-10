2566cfdadbe1958a8cd6707abb200974f11dbb27
kpdbos <kpdbos@gmail.com>
2021-12-10 02:10:22 -0500

    Heretic Pauldron + texture fix

--- Addons/textures/101st_armor.hpp
+++ Addons/textures/101st_armor.hpp
@@ -609,6 +609,28 @@ class 101st_Seatie_Pauldron: V_RebreatherB
 		uniformType="Default";
 		mass=80;
 		vestType = "Rebreather";
+		uniformModel = "\MRC\JLTS\characters\CloneArmor\CloneVestOfficer2.p3d";
+		hiddenSelections[] = {"camo1"};
+	};
+};
+class 101st_Heretic_Pauldron: V_RebreatherB
+{
+	author="Dutch";
+	dlc = "101st_Aux_Mod";
+	scope=1;
+	scopeArsenal=2;
+	displayName="101st Heretic Pauldron (JLTS)";
+	picture = "\MRC\JLTS\characters\CloneArmor\data\ui\CloneVestPauldron_ui_ca.paa";
+	hiddenSelectionsTextures[]=
+	{
+		"\101st_Aux_Mod\Addons\textures\101st\armor\pauldrons\101st_Heretic_Pauldron_CO.paa"
+	};
+	class ItemInfo: VestItem
+	{
+		containerClass="Supply100";
+		uniformType="Default";
+		mass=80;
+		vestType = "Rebreather";
 		uniformModel = "\MRC\JLTS\characters\CloneArmor\CloneVestCommander.p3d";
 		hiddenSelections[] = {"camo1"};
 	};
