ede709db32222b8d8d6b38a2b63828694140df6d
Matthew Horger <mhorger3@gmail.com>
2021-12-19 19:20:08 -0500

    Fixing some helms paths

--- Addons/DBA_Republic/DBA_Armour/DBA_Phase2/Helmet/FinalModel/config.cpp
+++ Addons/DBA_Republic/DBA_Armour/DBA_Phase2/Helmet/FinalModel/config.cpp
@@ -18,42 +18,45 @@ class cfgPatches
 
 class cfgWeapons
 {
-	class ItemCore;
-	class H_HelmetB : ItemCore
+	class H_HelmetSpecB;
+	class HeadgearItem: InventoryItem_Base_F
 	{
-		class ItemInfo;
+		allowedSlots[]={901,605};
+		type=605;
+		hiddenSelections[]={};
+		hitpointName="HitHead";
 	};
-	class DBA_P2HelmetBase : H_HelmetB
+	class DBA_P2HelmetBase : H_HelmetSpecB
 	{
 		author = "Vulgar";
 		displayName = "[DBA] P2 Helmet Base";
 		picture = "101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Helmet\Blankp2Arsenal_CA.paa";
 		//picture = "";
-		model = "101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Helmet\P2Helmet.p3d";
+		model = "101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Helmet\FinalModel\P2Helmet.p3d";
 		hiddenSelections[] = 
 		{
 			"camo"
 		};
 		hiddenSelectionsTextures[] = 
 		{
-			"101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Helmet\data\Trooper_Helmet_CO.paa"
+			"101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Helmet\FinalModel\data\Trooper_Helmet_CO.paa"
 		};
 		hiddenSelectionsMaterials[] =
 		{
-			"101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Helmet\data\Trooper_Helmet.rvmat"
+			"101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Helmet\FinalModel\data\Trooper_Helmet.rvmat"
 		};
-		class ItemInfo : ItemInfo
+		class ItemInfo : HeadgearItem
 		{
 			mass = 40;
-			uniformModel = "101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Helmet\P2Helmet.p3d";
+			uniformModel = "101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Helmet\FinalModel\P2Helmet.p3d";
 			hiddenSelections[] = {"camo"};
 			hiddenSelectionsTextures[] =
 			{
-				"101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Helmet\data\Trooper_Helmet_CO.paa"
+				"101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Helmet\FinalModel\data\Trooper_Helmet_CO.paa"
 			};
 			hiddenSelectionsMaterials[] =
 			{
-				"101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Helmet\data\Trooper_Helmet.rvmat"
+				"101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Helmet\FinalModel\data\Trooper_Helmet.rvmat"
 			};
 		};
 
@@ -73,7 +76,7 @@ class cfgWeapons
 		displayName = "DBA Phase 2 Clone Helmet: Rage";
 		hiddenSelectionsTextures[] =
 		{
-			"101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Helmet\data\Custom\Rage_Helmet_CO.paa"
+			"101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Helmet\FinalModel\data\Custom\Rage_Helmet_CO.paa"
 		};
 	};
 	class DBA_P2Helmet_Doom : DBA_P2HelmetBase
@@ -82,7 +85,7 @@ class cfgWeapons
 		displayName = "DBA Phase 2 Clone Helmet: DoomBot";
 		hiddenSelectionsTextures[] =
 		{
-			"101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Helmet\data\Custom\Doom_Guy_CO.paa"
+			"101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Helmet\FinalModel\data\Custom\Doom_Guy_CO.paa"
 		};
 	};
 	
