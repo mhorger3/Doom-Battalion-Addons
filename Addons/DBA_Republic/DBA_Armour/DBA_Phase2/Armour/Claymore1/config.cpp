807babef5215cb05c7d49de12731096bcee734b6
VulgarWorlock <82785789+VulgarWorlock@users.noreply.github.com>
2021-12-22 23:39:32 +0000

    Removing pointless dependencies

--- Addons/DBA_Republic/DBA_Armour/DBA_Phase2/Armour/Claymore1/config.cpp
+++ Addons/DBA_Republic/DBA_Armour/DBA_Phase2/Armour/Claymore1/config.cpp
@@ -6,18 +6,7 @@ class CfgPatches
 		name = "101st Aux Mod Custom Armor";
 		requiredaddons[] =
 		{
-			"A3_Weapons_F",
-			"A3_Functions_F",
-			"A3_Static_F_Jets_SAM_System_01",
-			"A3_Static_F_Jets_SAM_System_02",
-			"A3_Data_F_Destroyer",
-			"A3_Static_F_Destroyer",
-			"A3_Static_F_Destroyer_Ship_MRLS_01",
-			"OPTRE_Core",
-			"OPTRE_Weapons",
-			"OPTRE_Weapons_SMG",
-			"OPTRE_Weapons_Rockets",
-			"A3_Armor_F_EPB_MBT_03"
+			"A3_Characters_F"
 		};
 		requiredversion = 0.1;
 		units[] = {};
