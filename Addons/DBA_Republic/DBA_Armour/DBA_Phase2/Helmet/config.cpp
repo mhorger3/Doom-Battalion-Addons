021c1752a649def29a5ea87d9434795337a4ff07
VulgarWorlock <82785789+VulgarWorlock@users.noreply.github.com>
2022-01-11 02:20:58 +0000

    Fixes

--- Addons/DBA_Republic/DBA_Armour/DBA_Phase2/Helmet/config.cpp
+++ Addons/DBA_Republic/DBA_Armour/DBA_Phase2/Helmet/config.cpp
@@ -8,16 +8,6 @@ class CfgPatches
 		{
 			"A3_Weapons_F",
 			"A3_Functions_F",
-			"A3_Static_F_Jets_SAM_System_01", 
-			"A3_Static_F_Jets_SAM_System_02", 
-			"A3_Data_F_Destroyer",
-			"A3_Static_F_Destroyer", 
-			"A3_Static_F_Destroyer_Ship_MRLS_01", 
-			"OPTRE_Core", 
-			"OPTRE_Weapons", 
-			"OPTRE_Weapons_SMG",
-			"OPTRE_Weapons_Rockets", 
-			"A3_Armor_F_EPB_MBT_03"
 		};
 		requiredversion = 0.1;
 		units[]={};
