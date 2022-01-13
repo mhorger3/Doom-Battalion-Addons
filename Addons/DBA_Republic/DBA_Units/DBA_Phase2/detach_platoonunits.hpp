88ff042d361a371d82187e4984f751b1fe62554d
kpdbos <kpdbos@gmail.com>
2022-01-13 17:50:59 -0500

    Units upload

--- Addons/DBA_Republic/DBA_Units/DBA_Phase2/detach_platoonunits.hpp
+++ Addons/DBA_Republic/DBA_Units/DBA_Phase2/detach_platoonunits.hpp
@@ -1,8 +1,9 @@
-class CXC_DBA : 101st_JLTS_Clone_P2
+//Hammer
+class DBA_Reserve_Pilot_F : 101st_JLTS_Clone_P2
 {
 	dlc = "101st_Aux_Mod";
 	faction = "Republic_101st";
-	displayName = "101st Pilot CX-C (JLTS)";
+	displayName = "101st Pilot (Reserve)";
 	uniformClass = "DBA_CXC_Clean";
 	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
 	faction = "Republic_101st";
@@ -14,19 +15,19 @@ class CXC_DBA : 101st_JLTS_Clone_P2
 	};
 	hiddenSelectionsTextures[]=
 	{
-		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_CX-C_Clean_Chest_CO.paa.paa",
-		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_CX_Clean_Legs_CO.paa"
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Pilot_Reserve_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Pilot_Legs_CO.paa"
 	};
 	
 	scope = 2;
 	scopeCurator = 2;
 };
-class CX_DBA : 101st_JLTS_Clone_P2
+class DBA_11_Cadet_Pilot_F : 101st_JLTS_Clone_P2
 {
 	dlc = "101st_Aux_Mod";
 	faction = "Republic_101st";
-	displayName = "101st Pilot CX (JLTS)";
-	uniformClass = "DBA_CX_Clean";
+	displayName = "101st Cadet Pilot (1-1)";
+	uniformClass = "DBA_11_Cadet_Pilot";
 	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
 	faction = "Republic_101st";
 	editorSubcategory = "DBA_Claymore_HQ";
@@ -37,19 +38,19 @@ class CX_DBA : 101st_JLTS_Clone_P2
 	};
 	hiddenSelectionsTextures[]=
 	{
-		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_CX_Clean_Chest_CO.paa",
-		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_CX_Clean_Legs_CO.paa"
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Cadet_1-1_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Cadet_Legs_CO.paa"
 	};
 	
 	scope = 2;
 	scopeCurator = 2;
 };
-class CXX_DBA : 101st_JLTS_Clone_P2
+class DBA_12_Cadet_Pilot_F : 101st_JLTS_Clone_P2
 {
 	dlc = "101st_Aux_Mod";
 	faction = "Republic_101st";
-	displayName = "101st Pilot CX-T (JLTS)";
-	uniformClass = "DBA_CXX_Clean";
+	displayName = "101st Cadet Pilot (1-2)";
+	uniformClass = "DBA_12_Cadet_Pilot";
 	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
 	faction = "Republic_101st";
 	editorSubcategory = "DBA_Claymore_HQ";
@@ -60,19 +61,19 @@ class CXX_DBA : 101st_JLTS_Clone_P2
 	};
 	hiddenSelectionsTextures[]=
 	{
-		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_CX-X_Clean_Chest_CO.paa",
-		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_CX-X_Clean_Legs_CO.paa"
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Cadet_1-2_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Cadet_Legs_CO.paa"
 	};
 	
 	scope = 2;
 	scopeCurator = 2;
 };
-class CXP_DBA : 101st_JLTS_Clone_P2
+class DBA_21_Cadet_Pilot_F : 101st_JLTS_Clone_P2
 {
 	dlc = "101st_Aux_Mod";
 	faction = "Republic_101st";
-	displayName = "101st Pilot CX-P (JLTS)";
-	uniformClass = "DBA_CXP_Clean";
+	displayName = "101st Cadet Pilot (2-1)";
+	uniformClass = "DBA_21_Cadet_Pilot";
 	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
 	faction = "Republic_101st";
 	editorSubcategory = "DBA_Claymore_HQ";
@@ -83,20 +84,19 @@ class CXP_DBA : 101st_JLTS_Clone_P2
 	};
 	hiddenSelectionsTextures[]=
 	{
-		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_CX-P_Clean_Chest_CO.paa",
-		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_CX_Clean_Legs_CO.paa"
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Cadet_2-1_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Cadet_Legs_CO.paa"
 	};
 	
 	scope = 2;
 	scopeCurator = 2;
 };
-
-class CXS_DBA : 101st_JLTS_Clone_P2
+class DBA_22_Cadet_Pilot_F : 101st_JLTS_Clone_P2
 {
 	dlc = "101st_Aux_Mod";
 	faction = "Republic_101st";
-	displayName = "101st Pilot CX-S (JLTS)";
-	uniformClass = "DBA_CXS_Clean";
+	displayName = "101st Cadet Pilot (2-2)";
+	uniformClass = "DBA_22_Cadet_Pilot";
 	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
 	faction = "Republic_101st";
 	editorSubcategory = "DBA_Claymore_HQ";
@@ -107,19 +107,19 @@ class CXS_DBA : 101st_JLTS_Clone_P2
 	};
 	hiddenSelectionsTextures[]=
 	{
-		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_CX-S_Clean_Chest_CO.paa",
-		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_CX_Clean_Legs_CO.paa"
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Cadet_2-2_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Cadet_Legs_CO.paa"
 	};
 	
 	scope = 2;
 	scopeCurator = 2;
 };
-class CXO_DBA : 101st_JLTS_Clone_P2
+class DBA_31_Cadet_Pilot_F : 101st_JLTS_Clone_P2
 {
 	dlc = "101st_Aux_Mod";
 	faction = "Republic_101st";
-	displayName = "101st Pilot CX-O (JLTS)";
-	uniformClass = "DBA_CXO_Clean";
+	displayName = "101st Cadet Pilot (3-1)";
+	uniformClass = "DBA_31_Cadet_Pilot";
 	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
 	faction = "Republic_101st";
 	editorSubcategory = "DBA_Claymore_HQ";
@@ -130,13 +130,474 @@ class CXO_DBA : 101st_JLTS_Clone_P2
 	};
 	hiddenSelectionsTextures[]=
 	{
-		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_CX-O_Clean_Chest_CO.paa",
-		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_CX_Clean_Legs_CO.paa"
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Cadet_3-1_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Cadet_Legs_CO.paa"
 	};
 	
 	scope = 2;
 	scopeCurator = 2;
 };
+class DBA_11_Pilot_F : 101st_JLTS_Clone_P2
+{
+	dlc = "101st_Aux_Mod";
+	faction = "Republic_101st";
+	displayName = "101st Pilot (1-1)";
+	uniformClass = "DBA_11_Pilot";
+	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
+	faction = "Republic_101st";
+	editorSubcategory = "DBA_Claymore_HQ";
+	hiddenSelections[] =
+	{
+		"camo1",
+		"camo2"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Pilot_1-1_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Pilot_Legs_CO.paa"
+	};
+	
+	scope = 2;
+	scopeCurator = 2;
+};
+class DBA_12_Pilot_F : 101st_JLTS_Clone_P2
+{
+	dlc = "101st_Aux_Mod";
+	faction = "Republic_101st";
+	displayName = "101st Pilot (1-2)";
+	uniformClass = "DBA_11_Pilot";
+	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
+	faction = "Republic_101st";
+	editorSubcategory = "DBA_Claymore_HQ";
+	hiddenSelections[] =
+	{
+		"camo1",
+		"camo2"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Pilot_1-2_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Pilot_Legs_CO.paa"
+	};
+	
+	scope = 2;
+	scopeCurator = 2;
+};
+class DBA_21_Pilot_F : 101st_JLTS_Clone_P2
+{
+	dlc = "101st_Aux_Mod";
+	faction = "Republic_101st";
+	displayName = "101st Pilot (2-1)";
+	uniformClass = "DBA_21_Pilot";
+	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
+	faction = "Republic_101st";
+	editorSubcategory = "DBA_Claymore_HQ";
+	hiddenSelections[] =
+	{
+		"camo1",
+		"camo2"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Pilot_2-1_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Pilot_Legs_CO.paa"
+	};
+	
+	scope = 2;
+	scopeCurator = 2;
+};
+class DBA_22_Pilot_F : 101st_JLTS_Clone_P2
+{
+	dlc = "101st_Aux_Mod";
+	faction = "Republic_101st";
+	displayName = "101st Pilot (2-2)";
+	uniformClass = "DBA_22_Pilot";
+	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
+	faction = "Republic_101st";
+	editorSubcategory = "DBA_Claymore_HQ";
+	hiddenSelections[] =
+	{
+		"camo1",
+		"camo2"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Pilot_2-2_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Pilot_Legs_CO.paa"
+	};
+	
+	scope = 2;
+	scopeCurator = 2;
+};
+class DBA_31_Pilot_F : 101st_JLTS_Clone_P2
+{
+	dlc = "101st_Aux_Mod";
+	faction = "Republic_101st";
+	displayName = "101st Pilot (3-1)";
+	uniformClass = "DBA_31_Pilot";
+	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
+	faction = "Republic_101st";
+	editorSubcategory = "DBA_Claymore_HQ";
+	hiddenSelections[] =
+	{
+		"camo1",
+		"camo2"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Pilot_3-1_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Pilot_Legs_CO.paa"
+	};
+	
+	scope = 2;
+	scopeCurator = 2;
+};
+class DBA_11_VTOL_Pilot_F : 101st_JLTS_Clone_P2
+{
+	dlc = "101st_Aux_Mod";
+	faction = "Republic_101st";
+	displayName = "101st VTOL Pilot (1-1)";
+	uniformClass = "DBA_11_VTOL_Pilot";
+	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
+	faction = "Republic_101st";
+	editorSubcategory = "DBA_Claymore_HQ";
+	hiddenSelections[] =
+	{
+		"camo1",
+		"camo2"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_VTOL_1-1_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Pilot_Legs_CO.paa"
+	};
+	
+	scope = 2;
+	scopeCurator = 2;
+};
+class DBA_12_VTOL_Pilot_F : 101st_JLTS_Clone_P2
+{
+	dlc = "101st_Aux_Mod";
+	faction = "Republic_101st";
+	displayName = "101st VTOL Pilot (1-2)";
+	uniformClass = "DBA_12_VTOL_Pilot";
+	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
+	faction = "Republic_101st";
+	editorSubcategory = "DBA_Claymore_HQ";
+	hiddenSelections[] =
+	{
+		"camo1",
+		"camo2"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_VTOL_1-2_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Pilot_Legs_CO.paa"
+	};
+	
+	scope = 2;
+	scopeCurator = 2;
+};
+class DBA_21_VTOL_Pilot_F : 101st_JLTS_Clone_P2
+{
+	dlc = "101st_Aux_Mod";
+	faction = "Republic_101st";
+	displayName = "101st VTOL Pilot (2-1)";
+	uniformClass = "DBA_21_VTOL_Pilot";
+	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
+	faction = "Republic_101st";
+	editorSubcategory = "DBA_Claymore_HQ";
+	hiddenSelections[] =
+	{
+		"camo1",
+		"camo2"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_VTOL_2-1_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Pilot_Legs_CO.paa"
+	};
+	
+	scope = 2;
+	scopeCurator = 2;
+};
+class DBA_22_VTOL_Pilot_F : 101st_JLTS_Clone_P2
+{
+	dlc = "101st_Aux_Mod";
+	faction = "Republic_101st";
+	displayName = "101st VTOL Pilot (2-2)";
+	uniformClass = "DBA_22_VTOL_Pilot";
+	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
+	faction = "Republic_101st";
+	editorSubcategory = "DBA_Claymore_HQ";
+	hiddenSelections[] =
+	{
+		"camo1",
+		"camo2"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_VTOL_2-2_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Pilot_Legs_CO.paa"
+	};
+	
+	scope = 2;
+	scopeCurator = 2;
+};
+class DBA_31_VTOL_Pilot_F : 101st_JLTS_Clone_P2
+{
+	dlc = "101st_Aux_Mod";
+	faction = "Republic_101st";
+	displayName = "101st VTOL Pilot (3-1)";
+	uniformClass = "DBA_31_VTOL_Pilot";
+	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
+	faction = "Republic_101st";
+	editorSubcategory = "DBA_Claymore_HQ";
+	hiddenSelections[] =
+	{
+		"camo1",
+		"camo2"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_VTOL_3-1_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Pilot_Legs_CO.paa"
+	};
+	
+	scope = 2;
+	scopeCurator = 2;
+};
+class DBA_11_Bravo_Pilot_F : 101st_JLTS_Clone_P2
+{
+	dlc = "101st_Aux_Mod";
+	faction = "Republic_101st";
+	displayName = "101st Bravo Pilot (1-1)";
+	uniformClass = "DBA_11_Bravo_Pilot";
+	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
+	faction = "Republic_101st";
+	editorSubcategory = "DBA_Claymore_HQ";
+	hiddenSelections[] =
+	{
+		"camo1",
+		"camo2"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_2IC_1-1_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_NCO_Legs_CO.paa"
+	};
+	
+	scope = 2;
+	scopeCurator = 2;
+};
+class DBA_12_Bravo_Pilot_F : 101st_JLTS_Clone_P2
+{
+	dlc = "101st_Aux_Mod";
+	faction = "Republic_101st";
+	displayName = "101st Bravo Pilot (1-2)";
+	uniformClass = "DBA_12_Bravo_Pilot";
+	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
+	faction = "Republic_101st";
+	editorSubcategory = "DBA_Claymore_HQ";
+	hiddenSelections[] =
+	{
+		"camo1",
+		"camo2"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_2IC_1-2_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_NCO_Legs_CO.paa"
+	};
+	
+	scope = 2;
+	scopeCurator = 2;
+};
+class DBA_21_Bravo_Pilot_F : 101st_JLTS_Clone_P2
+{
+	dlc = "101st_Aux_Mod";
+	faction = "Republic_101st";
+	displayName = "101st Bravo Pilot (2-1)";
+	uniformClass = "DBA_21_Bravo_Pilot";
+	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
+	faction = "Republic_101st";
+	editorSubcategory = "DBA_Claymore_HQ";
+	hiddenSelections[] =
+	{
+		"camo1",
+		"camo2"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_2IC_2-1_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_NCO_Legs_CO.paa"
+	};
+	
+	scope = 2;
+	scopeCurator = 2;
+};
+class DBA_22_Bravo_Pilot_F : 101st_JLTS_Clone_P2
+{
+	dlc = "101st_Aux_Mod";
+	faction = "Republic_101st";
+	displayName = "101st Bravo Pilot (2-2)";
+	uniformClass = "DBA_22_Bravo_Pilot";
+	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
+	faction = "Republic_101st";
+	editorSubcategory = "DBA_Claymore_HQ";
+	hiddenSelections[] =
+	{
+		"camo1",
+		"camo2"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_2IC_2-2_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_NCO_Legs_CO.paa"
+	};
+	
+	scope = 2;
+	scopeCurator = 2;
+};
+class DBA_31_Bravo_Pilot_F : 101st_JLTS_Clone_P2
+{
+	dlc = "101st_Aux_Mod";
+	faction = "Republic_101st";
+	displayName = "101st Bravo Pilot (3-1)";
+	uniformClass = "DBA_31_Bravo_Pilot";
+	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
+	faction = "Republic_101st";
+	editorSubcategory = "DBA_Claymore_HQ";
+	hiddenSelections[] =
+	{
+		"camo1",
+		"camo2"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_2IC_3-1_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_NCO_Legs_CO.paa"
+	};
+	
+	scope = 2;
+	scopeCurator = 2;
+};
+class DBA_11_Alpha_Pilot_F : 101st_JLTS_Clone_P2
+{
+	dlc = "101st_Aux_Mod";
+	faction = "Republic_101st";
+	displayName = "101st Alpha Pilot (1-1)";
+	uniformClass = "DBA_11_Alpha_Pilot";
+	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
+	faction = "Republic_101st";
+	editorSubcategory = "DBA_Claymore_HQ";
+	hiddenSelections[] =
+	{
+		"camo1",
+		"camo2"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Lead_1-1_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_NCO_Legs_CO.paa"
+	};
+	
+	scope = 2;
+	scopeCurator = 2;
+};
+class DBA_12_Alpha_Pilot_F : 101st_JLTS_Clone_P2
+{
+	dlc = "101st_Aux_Mod";
+	faction = "Republic_101st";
+	displayName = "101st Alpha Pilot (1-2)";
+	uniformClass = "DBA_12_Alpha_Pilot";
+	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
+	faction = "Republic_101st";
+	editorSubcategory = "DBA_Claymore_HQ";
+	hiddenSelections[] =
+	{
+		"camo1",
+		"camo2"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Lead_1-2_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_NCO_Legs_CO.paa"
+	};
+	
+	scope = 2;
+	scopeCurator = 2;
+};
+class DBA_21_Alpha_Pilot_F : 101st_JLTS_Clone_P2
+{
+	dlc = "101st_Aux_Mod";
+	faction = "Republic_101st";
+	displayName = "101st Alpha Pilot (2-1)";
+	uniformClass = "DBA_21_Alpha_Pilot";
+	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
+	faction = "Republic_101st";
+	editorSubcategory = "DBA_Claymore_HQ";
+	hiddenSelections[] =
+	{
+		"camo1",
+		"camo2"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Lead_2-1_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_NCO_Legs_CO.paa"
+	};
+	
+	scope = 2;
+	scopeCurator = 2;
+};
+class DBA_22_Alpha_Pilot_F : 101st_JLTS_Clone_P2
+{
+	dlc = "101st_Aux_Mod";
+	faction = "Republic_101st";
+	displayName = "101st Alpha Pilot (2-2)";
+	uniformClass = "DBA_22_Alpha_Pilot";
+	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
+	faction = "Republic_101st";
+	editorSubcategory = "DBA_Claymore_HQ";
+	hiddenSelections[] =
+	{
+		"camo1",
+		"camo2"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Lead_2-2_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_NCO_Legs_CO.paa"
+	};
+	
+	scope = 2;
+	scopeCurator = 2;
+};
+class DBA_31_Alpha_Pilot_F : 101st_JLTS_Clone_P2
+{
+	dlc = "101st_Aux_Mod";
+	faction = "Republic_101st";
+	displayName = "101st Alpha Pilot (3-1)";
+	uniformClass = "DBA_31_Alpha_Pilot";
+	model = "\MRC\JLTS\characters\CloneArmor\CloneArmor.p3d";
+	faction = "Republic_101st";
+	editorSubcategory = "DBA_Claymore_HQ";
+	hiddenSelections[] =
+	{
+		"camo1",
+		"camo2"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_Lead_3-1_Chest_CO.paa",
+		"\101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Armour\data\Custom\Hammer\DBA_NCO_Legs_CO.paa"
+	};
+	
+	scope = 2;
+	scopeCurator = 2;
+};
+//Dagger
 class Dagger_DBA : 101st_JLTS_Clone_P2
 {
 	dlc = "101st_Aux_Mod";
@@ -252,6 +713,7 @@ class Dagger_RTO_DBA : 101st_JLTS_Clone_P2
 	scope = 2;
 	scopeCurator = 2;
 };
+//Longbow
 class Longbow_DBA : 101st_JLTS_Clone_P2
 {
 	dlc = "101st_Aux_Mod";
