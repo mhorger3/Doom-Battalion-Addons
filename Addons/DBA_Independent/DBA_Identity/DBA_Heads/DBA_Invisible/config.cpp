2ac2afd138876cb311d931463e5f9d699e591c67
101st-Morn <bindingofwilson@gmail.com>
2022-01-25 21:55:25 -0600

    Invisible Head

---
+++ Addons/DBA_Independent/DBA_Identity/DBA_Heads/DBA_Invisible/config.cpp
@@ -0,0 +1,40 @@
+class CfgPatches
+{
+	class DBA_Invisible
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0;
+		requiredAddons[]=
+		{
+			"A3_Characters_F_Heads"
+		};
+		version="1.0";
+		author="Morn";
+	};
+};
+class CfgHeads
+{
+	class DefaultHead_A3;
+	class DBA_Invisible: DefaultHead_A3
+	{
+		model=""
+	};
+};
+class CfgFaces
+{
+	class Default;
+	class Man_A3: Default
+	{
+		class DBA_Invisible
+		{
+			author="Morn";
+			head="DBA_Invisible";
+			identityTypes[]={};
+			displayname="No Head";
+			texture=""
+			material = "";
+			disabled=0;
+		};
+	};
+};
