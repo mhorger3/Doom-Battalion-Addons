7dbb3bca6de5aa910c5309813f1b5ac7e1eee101
kpdbos <kpdbos@gmail.com>
2022-03-04 23:08:57 -0500

    Kodiak

--- Addons/DBA_Republic/DBA_Armour/DBA_Phase2/Helmet/config.cpp
+++ Addons/DBA_Republic/DBA_Armour/DBA_Phase2/Helmet/config.cpp
@@ -3371,7 +3371,7 @@ class CfgWeapons
 		picture = "\MRC\JLTS\characters\CloneArmor\data\ui\CloneHelmetP2_ui_ca.paa";
 		hiddenSelectionsTextures[]=
 		{
-			"101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Customs\Kodiak_Helmet_CO.paa"
+			"101st_Aux_Mod\Addons\DBA_Republic\DBA_Armour\DBA_Phase2\Helmet\Pilot\Zelph_Pilot_Helmet_CO.paa"
 		};
 	};
 	class 101st_Deadeye_Helmet: JLTS_CloneHelmetP2
