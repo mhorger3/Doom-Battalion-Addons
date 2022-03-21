c679468e995da0af8a61997c8ff8587d3807161b
ISU <30844658+ISU152m3@users.noreply.github.com>
2022-03-21 14:53:27 -0700

    pt2

--- Addons/DBA_Republic/Addons/DBA_Camo_Changer/CfgEventHandlers.hpp
+++ Addons/DBA_Republic/Addons/DBA_Camo_Changer/CfgEventHandlers.hpp
@@ -2,7 +2,7 @@ class Extended_PreInit_EventHandlers
 {
     class DBA_Camo_Changer_pre_init_event
 	{
-        init = "call compile preprocessFileLineNumbers 'DBA_Republic\Addons\DBA_General\DBA_Camo_Changer\XEH_preInit.sqf'";
+        init = "call compile preprocessFileLineNumbers 'DBA_Republic\Addons\DBA_Camo_Changer\XEH_preInit.sqf'";
     };
 };
 
@@ -10,6 +10,6 @@ class Extended_PostInit_EventHandlers
 {
     class DBA_Camo_Changer_post_init_event
 	{
-        init = "call compile preprocessFileLineNumbers 'DBA_Republic\Addons\DBA_General\DBA_Camo_Changer\XEH_postInit.sqf'";
+        init = "call compile preprocessFileLineNumbers 'DBA_Republic\Addons\DBA_Camo_Changer\XEH_postInit.sqf'";
     };
 };
